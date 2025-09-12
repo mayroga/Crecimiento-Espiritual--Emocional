4943e40ebdc865e1c3706445bc31fe2f855510be
mayroga <132472344+mayroga@users.noreply.github.com>
2025-09-12 17:52:29 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+async function sendMessage(){
+    const msgInput = document.getElementById("message");
+    const msg = msgInput.value;
+    if(!msg) return;
+    const chatBox = document.getElementById("chat-box");
+    chatBox.innerHTML += `<p><b>Tú:</b> ${msg}</p>`;
+    msgInput.value = "";
+
+    const res = await fetch("/chat", {
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body: JSON.stringify({message: msg})
+    });
+    const data = await res.json();
+    chatBox.innerHTML += `<p><b>Guía:</b> ${data.reply}</p>`;
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
